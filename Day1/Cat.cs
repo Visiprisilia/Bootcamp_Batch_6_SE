9459d974431fef684947bdd1ea932225309057c4
Visi Prisilia Pananginan <visiprisiliapananginan@gmail.com>
2023-10-10 00:44:47 +0700

    first commit

---
+++ Day1/Cat.cs
@@ -0,0 +1,32 @@
+namespace Animal;
+public class Cat
+
+{
+	public string name;
+	public string colour;
+	public int leg;
+	public bool istaillong;
+	public int weight;
+	public int height;
+
+	public void Jump()
+	{
+		Console.WriteLine("Jump");
+	}
+	public void Run()
+	{
+		Console.WriteLine("Run");
+	}
+	public void Eat()
+	{
+		Console.WriteLine("Eat");
+	}
+	public void Drop()
+	{
+		Console.WriteLine("Drop");
+	}
+	public void Sleep()
+	{
+		Console.WriteLine("Sleep");
+	}
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿using Animal;
+class Program
+
+{
+	 static void Main(string[] args)
+
+	{
+		
+	Cat oyen = new Cat();
+	
+	oyen.name = "Oyen";
+	oyen.colour = "Orange";
+	oyen.leg = 4;
+	oyen.istaillong = true;
+	oyen.height = 50;
+	oyen.weight = 20;
+	
+	Console.WriteLine(oyen.name);	
+	Console.WriteLine(oyen.colour);	
+	Console.WriteLine(oyen.leg);	
+	Console.WriteLine(oyen.istaillong);	
+	Console.WriteLine(oyen.weight);	
+	Console.WriteLine(oyen.height);	
+	
+	oyen.Jump();
+	
+	Console.WriteLine();
+	
+	Cat bambang = new Cat();
+	
+	bambang.name = "Bambang";
+	bambang.colour = "Black";
+	bambang.leg = 4;
+	bambang.istaillong = true;
+	bambang.height = 40;
+	bambang.weight = 30;
+	
+	Console.WriteLine(bambang.name);	
+	Console.WriteLine(bambang.colour);	
+	Console.WriteLine(bambang.leg);	
+	Console.WriteLine(bambang.istaillong);	
+	Console.WriteLine(bambang.weight);	
+	Console.WriteLine(bambang.height);	
+	
+		
+	}
+}
