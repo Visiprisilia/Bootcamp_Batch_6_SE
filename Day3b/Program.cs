26a1df1655144f8bce2e0a6eca74621c1a875076
VisiPrisilia <visiprisiliapananginan@gmail.com>
2023-10-11 15:29:05 +0700

    Overloading

---
+++ Day3b/Program.cs
@@ -0,0 +1,25 @@
+﻿//Overloading
+class Program {
+	static void Main() {
+		Perhitungan hitung = new();
+		hitung.Add();
+		hitung.Add(5,4);
+		hitung.Add(1,2,3);
+		hitung.Add("1","2");
+		Console.WriteLine(hitung.Add("1","2"));
+	}
+}
+class Perhitungan {
+	public void Add() {
+		Console.WriteLine("Tidak ada");
+	}
+	public void Add(int a, int b) {
+		Console.WriteLine($"Hasil Perkalian {a*b}");
+	}
+	public void Add(int a, int b, int c) {
+		Console.WriteLine($"Hasil Perhitungan {a*b+c}");
+	}
+	public string Add(string a, string b) {
+		return a+b;
+	}
+}
