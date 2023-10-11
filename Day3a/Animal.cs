66507f134055b5e2322e2c504ee3d4e8016f0fea
VisiPrisilia <visiprisiliapananginan@gmail.com>
2023-10-11 16:04:05 +0700

    Overloading

---
+++ Day3a/Animal.cs
@@ -0,0 +1,14 @@
+namespace LivingBeing;
+
+public class Animal{
+	
+	public string name;
+	public int age;
+	public Animal(string name, int age) 
+	{
+		// Console.WriteLine($"Animal name is {name} instance created");
+		// Console.WriteLine($"Animal age is {age} instance created");
+		this.name = name;
+		this.age = age;
+	}
+}
---
+++ Day3a/Program.cs
@@ -0,0 +1,14 @@
+﻿using LivingBeing;
+
+class Program 
+{
+	static void Main(){
+		Cat cat = new Cat("oyen",4);
+		Console.WriteLine(cat.name);
+		Console.WriteLine(cat.age);
+		// Console.WriteLine(cat.GetAge());
+		cat.SetAge(4);
+		
+	}
+}
+	
