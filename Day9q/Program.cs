a47481a084de1813f6fe0b5abdf7659663c32435
VisiPrisilia <visiprisiliapananginan@gmail.com>
2023-10-23 16:42:27 +0700

    day9

---
+++ Day9q/Program.cs
@@ -0,0 +1,9 @@
+﻿class program
+{
+	static void Main()
+	{
+		// DateTime date1 = new DateTime(2023, 10, 23, 15,02,00);
+		// Console.WriteLine(date1.ToString());
+		Console.WriteLine(DateTime.MaxValue);
+	}
+}
