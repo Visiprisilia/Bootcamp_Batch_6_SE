56e08195bec5c9454a46e9521f6eead8eb8fb929
Visiprisilia <visiprisiliapananginan@gmail.com>
2023-11-06 14:12:34 +0700

    thread and task

---
+++ Day14/N_TaskReturnValue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		int x = 3;
+		int y = 4;
+		Task<int> task = Task.Run(()=>Add(x,y)); //buat generik untuk valuenya (int)
+		// task.Wait();
+		int result = task.Result; //panggil hasil
+		Console.WriteLine($"Result: {result}");
+	}
+	
+	static int Add(int a, int b)
+	{
+		// task.Delay(200) //task
+		// task.Sleep(200) //thread
+		return a + b;
+	}
+}
